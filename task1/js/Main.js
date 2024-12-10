4b41b7081d07c93b50db6d2c6edd906f634b74c3
rahulmi2025 <er.rahulmi@gmail.com>
2024-12-10 21:11:06 +0545

    Add files via upload

---
+++ task1/js/Main.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper', {
+  
+
+  
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    direction: "vertical",
+    slidesPerView: 1,
+    spaceBetween: 30,
+    mousewheel: true,
+  });
